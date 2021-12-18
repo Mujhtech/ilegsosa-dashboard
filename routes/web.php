9aff3ccf769fa1016165d8829515f0043a22162c
Mujhtech <mujeeb.muhideen@gmail.com>
2021-12-18 14:54:45 +0100

    setup mail service

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\PaymentTypeController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VoteController;
+use App\Mail\TestAmazonSes;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,6 +28,10 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get('/storage-link', function () {
     Artisan::call('storage:link');
+});
+
+Route::get('test', function () {
+    Mail::to('mujhtech@gmail.com')->send(new TestAmazonSes('It work'));
 });
 
 Route::get('/paystack/handle/callback', [PaymentController::class, 'handleCallback']);
